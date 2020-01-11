7463567ab81af44e0e3d0cdc5955ab53cf290384
creebe <59752335+creebe@users.noreply.github.com>
2020-01-10 19:54:01 -0500

    Add files via upload

---
+++ ProceduralRing.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+public class ProceduralRing : MonoBehaviour
+{
+    public enum SubdivisionType
+    {
+        Constant,
+        AngleRatio
+    }
+
+    public float InnerRadius = 0f;
+    public float OuterRadius = 2f;
+
+    public SubdivisionType SubdivisionMode;
+    [Range(0, 360)]
+    public float Angle = 45;
+    public int Subdivisions = 36;
+    public bool UpdateMeshCollider;
+
+    private Mesh mesh;
+    private MeshCollider meshCollider;
+
+    private Vector3[] vertices;
+    private int[] indices;
+    private float angleDelta = 10f;
+    public float angleAdjustor;
+    private float growAndShrinkSpeed = 12f;
+
+    private void Start()
+    {
+        angleAdjustor = -1 * (angleDelta / 2);
+    }
+
+    void Update()
+    {
+        ChangeRingSize();
+    }
+
+    public void UpdateMesh()
+    {
+        updateMesh();
+    }
+
+    void OnValidate()
+    {
+        updateMesh();
+    }
+
+    void updateMesh()
+    {
+        if (mesh == null)
+        {
+            mesh = new Mesh();
+            mesh.MarkDynamic();
+            mesh.name = "ProceduralRing";
+            updateMesh();
+
+            GetComponent<MeshFilter>().mesh = mesh;
+        }
+
+        int subdivisions = (SubdivisionMode == SubdivisionType.AngleRatio) ? Mathf.CeilToInt(Angle / 360.0f * Subdivisions) : Subdivisions;
+
+        int vertexCount = (Mathf.CeilToInt(subdivisions + 1)) * 2;
+        if (vertices == null || vertices.Length != vertexCount)
+        {
+            vertices = new Vector3[vertexCount];
+        }
+
+        float dAlpha = 2 * Mathf.PI / 360 * Angle / subdivisions;
+        for (int i = 0; i < subdivisions + 1; ++i)
+        {
+            float angle = dAlpha * i;
+            float nx = Mathf.Cos(angle);
+            float ny = Mathf.Sin(angle);
+
+            vertices[2 * i] = InnerRadius * new Vector3(nx, ny, 0);
+            vertices[2 * i + 1] = OuterRadius * new Vector3(nx, ny, 0);
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+
+        int indexCount = 3 * subdivisions * 2;
+        if (indices == null || indices.Length != indexCount)
+        {
+            indices = new int[3 * subdivisions * 2];
+        }
+
+        for (int i = 0; i < subdivisions; ++i)
+        {
+            indices[6 * i] = 2 * i + 1;
+            indices[6 * i + 1] = 2 * i;
+            indices[6 * i + 2] = 2 * i + 2;
+            indices[6 * i + 3] = 2 * i + 3;
+            indices[6 * i + 4] = 2 * i + 1;
+            indices[6 * i + 5] = 2 * i + 2;
+        }
+
+        mesh.SetIndices(indices, MeshTopology.Triangles, 0);
+
+        if (UpdateMeshCollider)
+        {
+            if (meshCollider == null)
+            {
+                meshCollider = GetComponent<MeshCollider>();
+            }
+            meshCollider.sharedMesh = mesh;
+        }
+    }
+
+    void ChangeRingSize()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Angle += angleDelta;
+            updateMesh();
+
+            Quaternion rotation = Quaternion.AngleAxis(angleAdjustor, Vector3.forward);
+            transform.rotation *= rotation;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Angle -= angleDelta;
+            updateMesh();
+
+            Quaternion rotation = Quaternion.AngleAxis(angleAdjustor, -Vector3.forward);
+            transform.rotation *= rotation;
+        }
+    }
+}
---
+++ Rotate.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    //variables for MagicOne
+    private GameObject magicOneParentGO;
+    private GameObject dialOneGO;
+    public ProceduralRing magicOneChildPR;
+    private float offSetAngleForMagicOne;
+    private bool touchOne;
+    private MeshCollider magicOneChildMC;
+    private MeshRenderer magicOneChildMR;
+
+    [SerializeField]
+    private float upperRotateYLimit = 500f;
+
+    private int midPoint = 380;
+    private float rangeForDialActivation = 1.5f;
+
+    private void Start()
+    {
+        //caching for MagicOne
+        magicOneParentGO = GameObject.Find("MagicOneParent");
+        dialOneGO = GameObject.Find("DialOne");
+        magicOneChildPR = GameObject.Find("MagicOneChild").GetComponent<ProceduralRing>();
+        offSetAngleForMagicOne = -1 * (magicOneChildPR.Angle / 2);
+        magicOneChildMC = GameObject.Find("MagicOneChild").GetComponent<MeshCollider>();
+        magicOneChildMR = GameObject.Find("MagicOneChild").GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        ManageTouchOne();
+    }
+
+    void ManageTouchOne()
+    {
+        float distfromDialOne = Vector2.Distance(Camera.main.ScreenToWorldPoint(Input.mousePosition), dialOneGO.transform.position);
+
+        if (distfromDialOne < rangeForDialActivation)
+        {
+            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - dialOneGO.transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            Quaternion rotation = Quaternion.AngleAxis(offSetAngleForMagicOne, Vector3.forward) * Quaternion.AngleAxis(angle, Vector3.forward);
+            magicOneParentGO.transform.rotation = rotation;
+        }
+    }
+}
